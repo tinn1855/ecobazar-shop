8d111b40b3eee9ec4c0bd8e1c650492fb3c4d96e
tinnguyen <tinn1855@gmail.com>
2025-06-17 09:06:02 +0700

    build design system

--- src/components/design-system/index.tsx
+++ src/components/design-system/index.tsx
@@ -1,3 +1,23 @@
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
+
 export function DesignSystem() {
-  return <div>Design System</div>;
+  return (
+    <div className="container mx-auto p-4 flex flex-col gap-4">
+      <div className="flex  items-center gap-2">
+        <div className="w-24">Buttton:</div>
+        <div className="flex gap-2">
+          <Button>Primary Button</Button>
+          <Button variant={'secondary'}>Secondary Button</Button>
+          <Button>Search Button</Button>
+        </div>
+      </div>
+      <div className="flex  items-center gap-2">
+        <div className="w-24">Input:</div>
+        <div className="flex gap-2">
+          <Input />
+        </div>
+      </div>
+    </div>
+  );
 }
